d7e28446fc8114a28b84e777240f0dd5b104898b
Guilherme Andrade e Chaves <guilherme@live.it>
2021-07-10 10:42:35 -0300

    Adicionado o texto da sessão 2

--- src/views/components/sections/homeSectionTwo.js
+++ src/views/components/sections/homeSectionTwo.js
@@ -1,10 +1,21 @@
 import background2 from '../../../assets//images/background/background2.png';
 import darthvader from '../../../assets/images/elements/vader.png'
 
+let TextContent = `
+Lucas ipsum dolor sit amet luke sidious jango cade mace palpatine jawa wookiee wookiee kashyyyk. Dantooine calrissian r2-d2 calamari coruscant skywalker ewok. Darth fett moff endor twi'lek windu obi-wan luuke. Alderaan hutt hoth moff zabrak moff organa calamari. Ackbar mustafar wedge padmé mandalorians solo secura moff jar. Jawa luuke hutt organa jango jinn. Boba jabba kenobi moff luuke qui-gonn thrawn. Twi'lek yoda mace luke vader boba. Mace kessel darth antilles jabba. Luke hoth organa leia anakin skywalker leia ponda.
+
+Moff ahsoka yavin calamari. Chewbacca skywalker yoda organa kit organa. Hutt hutt twi'lek binks. Amidala darth lando mon. Grievous solo secura mandalore bespin chewbacca sebulba cade. Gamorrean palpatine organa organa yoda hutt antilles. C-3p0 luke skywalker luke mara mara gonk antilles gamorrean. Mara moff wookiee jinn dantooine skywalker qui-gon. Kashyyyk c-3po watto organa. Droid tusken raider aayla moff thrawn darth tusken raider. Binks cade antilles ahsoka. Yoda c-3p0 dantooine antilles secura organa calamari.
+`
+
 let HomeSectioTwo = `
 <section class="section-two" section .background--two style="background-image: url(${background2})">
  <div>
  <img src="${darthvader}" alt="Vader" width="300px" heigth="auto"/>
+ <div>
+ <p>
+ ${TextContent}
+ </p>
+ </div>
  </div>
 </section>
 `
