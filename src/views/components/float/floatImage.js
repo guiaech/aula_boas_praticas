75513ea7414ffb9e986e1288fb7820938ad25525
Guilherme Andrade e Chaves <guilherme@live.it>
2021-07-10 10:37:01 -0300

    Adicionado a imagem do dath vader ao projeto na sessão 2

--- src/views/components/float/floatImage.js
+++ src/views/components/float/floatImage.js
@@ -1,7 +1,7 @@
 import yodaBaby from '../../../assets/images/elements/baby-yoda1.png'
 
 let floatImage = `
-<div class="main-left">
+<div class="main-left" width="120px" heigth="auto">
  <img src="${yodaBaby}"alt "Yoda Baby"/> 
 </div>
 `
--- src/views/components/sections/homeSectionTwo.js
+++ src/views/components/sections/homeSectionTwo.js
@@ -1,8 +1,11 @@
 import background2 from '../../../assets//images/background/background2.png';
+import darthvader from '../../../assets/images/elements/vader.png'
 
 let HomeSectioTwo = `
 <section class="section-two" section .background--two style="background-image: url(${background2})">
-<h2>Provisório</h2>
+ <div>
+ <img src="${darthvader}" alt="Vader" width="300px" heigth="auto"/>
+ </div>
 </section>
 `
 
