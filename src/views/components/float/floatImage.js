c3a22223002bf8e9bba67b851b6ba579b640dd39
Guilherme Andrade e Chaves <guilherme@live.it>
2021-07-10 15:25:10 -0300

    Finalizado responsavidade

--- src/views/components/float/floatImage.js
+++ src/views/components/float/floatImage.js
@@ -1,8 +1,10 @@
 import yodaBaby from '../../../assets/images/elements/baby-yoda1.png'
 
 let floatImage = `
-<div class="main-left" width="120px" heigth="auto">
- <img src="${yodaBaby}"alt "Yoda Baby"/> 
+<div class="main-left"clickOn="#section-two">
+    <a>
+        <img src="${yodaBaby}" alt="Yoda Baby">
+    </a>
 </div>
 `
 
